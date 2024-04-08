8b904858db6db1842b97dd12dec064773743a372
Hasan Taha Künkül <tahaknkl2323@gmail.com>
2024-04-08 11:33:54 +0300

    four commit

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,10 +1,67 @@
+import 'package:chat_app/auth/auth_service.dart';
+import 'package:chat_app/pages/settings.page.dart';
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  void _logout() {
+    //get auth service
+    final auth = AuthService();
+    auth.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer();
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        children: [
+          // logo and menu items
+          Column(
+            children: [
+              // logo
+              DrawerHeader(
+                child: Center(
+                  child: Icon(Icons.message, size: 64, color: Theme.of(context).colorScheme.primary),
+                ),
+              ),
+
+              // home list tile
+              ListTile(
+                title: const Text("H O M E"),
+                leading: const Icon(Icons.home),
+                onTap: () {
+                  Navigator.pop(context);
+                  // Handle home navigation
+                },
+              ),
+
+              // settings list tile
+              ListTile(
+                title: const Text("S E T T I N G S"),
+                leading: const Icon(Icons.settings),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingsPage()));
+                },
+              ),
+            ],
+          ),
+
+          // logout list tile
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: ListTile(
+                title: const Text("L O G O U T"),
+                leading: const Icon(Icons.logout),
+                onTap: _logout,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,32 +1,18 @@
-import 'package:chat_app/auth/auth_service.dart';
+import 'package:chat_app/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void _logout() {
-    //get auth service
-    final auth = AuthService();
-    auth.signOut();
-  }
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
-        actions: [
-          //logout button
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () {
-              //logout
-              _logout();
-            },
-          ),
-        ],
       ),
-      drawer: const Drawer(),
+      drawer: const MyDrawer(),
     );
   }
 }
